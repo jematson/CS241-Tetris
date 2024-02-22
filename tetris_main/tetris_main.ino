5511bf971204d8aab835166d58a62a611bec9864
Jenae Matson <jlmatson@alaska.edu>
2024-02-21 20:14:53 -0900

    feat: create tetris_main

---
+++ tetris_main/tetris_main.ino
@@ -0,0 +1,12 @@
+// CS 241 Final Project: Tetris Game
+// Jenae Matson and Elliott Lewandowski
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
