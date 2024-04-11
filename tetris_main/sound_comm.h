290fc91118063f252da71735e60aa894c5bc2b0e
Elliott Lewandowski <elewandowski30@gmail.com>
2024-04-11 15:05:45 -0800

    Swapped to falling edge comms

--- tetris_main/sound_comm.h
+++ tetris_main/sound_comm.h
@@ -4,13 +4,13 @@
 
 void playRowClear()
 {
-    digitalWrite(row_sound_pin, HIGH);
-    delay(1);
     digitalWrite(row_sound_pin, LOW);
+    delay(1);
+    digitalWrite(row_sound_pin, HIGH);
 }
 
 void playBlockDrop() {
-    digitalWrite(drop_sound_pin, HIGH);
-    delay(1);
     digitalWrite(drop_sound_pin, LOW);
+    delay(1);
+    digitalWrite(drop_sound_pin, HIGH);
 }
