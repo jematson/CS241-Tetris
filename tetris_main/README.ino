34d205adf4da76e7df1077cc18263b50ccf9193b
Jenae Matson <jlmatson@alaska.edu>
2024-04-10 19:40:37 -0800

    update todo list

--- tetris_main/README.ino
+++ tetris_main/README.ino
@@ -3,11 +3,14 @@ TODO
 
 - visual cue/animation when row clear
 
+- delay debris placement so you can slide
+
 - sound effects!
   - play the tetris music
   - fun noise when row clear
 
 - point board
   - 1 point per row cleared
+  - extra points for clearing 2 rows, and for clearing entire board
 
 */
