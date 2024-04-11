4e42584a1e86c418031a0c6800661bd4b6e3f3cc
Elliott Lewandowski <elewandowski30@gmail.com>
2024-04-11 15:03:37 -0800

    Set up comms function to work with a rising edge interupt

--- tetris_main/sound_comm.h
+++ tetris_main/sound_comm.h
@@ -4,9 +4,13 @@
 
 void playRowClear()
 {
-    
+    digitalWrite(row_sound_pin, HIGH);
+    delay(1);
+    digitalWrite(row_sound_pin, LOW);
 }
 
 void playBlockDrop() {
- 
+    digitalWrite(drop_sound_pin, HIGH);
+    delay(1);
+    digitalWrite(drop_sound_pin, LOW);
 }
