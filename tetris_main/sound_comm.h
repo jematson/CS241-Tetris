20546deb5f485a220380d7190e0a739629b6570e
Jenae Matson <jlmatson@alaska.edu>
2024-04-15 17:17:19 -0800

    Create pointboard.h for 7-segment display functions. Initialize sound pins

--- tetris_main/sound_comm.h
+++ tetris_main/sound_comm.h
@@ -6,15 +6,20 @@
 const int row_sound_pin = 6;
 const int drop_sound_pin = 5;
 
+void begin_sound_pins() {
+  pinMode(row_sound_pin, OUTPUT);
+  pinMode(drop_sound_pin, OUTPUT);
+}
+
 void playRowClear()
 {
     digitalWrite(row_sound_pin, LOW);
-    delay(1);
+    //delay(1);
     digitalWrite(row_sound_pin, HIGH);
 }
 
 void playBlockDrop() {
     digitalWrite(drop_sound_pin, LOW);
-    delay(1);
+    //delay(1);
     digitalWrite(drop_sound_pin, HIGH);
 }
