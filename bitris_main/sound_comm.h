93d3c23a8f84ecd136e4bf276b886fa0e5f25577
Jenae Matson <jlmatson@alaska.edu>
2024-04-26 14:29:38 -0800

    standardize variable names to camelCase

--- bitris_main/sound_comm.h
+++ bitris_main/sound_comm.h
@@ -3,42 +3,40 @@
 #include "data.h"
 
 //Sound comms pins
-const int row_sound_pin = 6;
-const int drop_sound_pin = 5;
-const int end_sound_pin = 4;
-const int song_sound_pin = 3;
+const int rowSoundPin = 6;
+const int dropSoundPin = 5;
+const int endSoundPin = 4;
+const int songSoundPin = 3;
 
 void begin_sound_pins() {
-  pinMode(row_sound_pin, OUTPUT);
-  pinMode(drop_sound_pin, OUTPUT);
-  pinMode(end_sound_pin, OUTPUT);
-  pinMode(song_sound_pin, OUTPUT);
-  digitalWrite(row_sound_pin, HIGH);
-  digitalWrite(drop_sound_pin, HIGH);
-  digitalWrite(end_sound_pin, HIGH);
-  digitalWrite(song_sound_pin, HIGH);
+  pinMode(rowSoundPin, OUTPUT);
+  pinMode(dropSoundPin, OUTPUT);
+  pinMode(endSoundPin, OUTPUT);
+  pinMode(songSoundPin, OUTPUT);
+  digitalWrite(rowSoundPin, HIGH);
+  digitalWrite(dropSoundPin, HIGH);
+  digitalWrite(endSoundPin, HIGH);
+  digitalWrite(songSoundPin, HIGH);
 }
 
 void playRowClear()
 {
-    digitalWrite(row_sound_pin, LOW);
-    //delay(1);
-    digitalWrite(row_sound_pin, HIGH);
+    digitalWrite(rowSoundPin, LOW);
+    digitalWrite(rowSoundPin, HIGH);
 }
 
 void playBlockDrop() {
-    digitalWrite(drop_sound_pin, LOW);
-    //delay(1);
-    digitalWrite(drop_sound_pin, HIGH);
+    digitalWrite(dropSoundPin, LOW);
+    digitalWrite(dropSoundPin, HIGH);
 }
 
 void playGameOver() {
-  digitalWrite(end_sound_pin, LOW);
+  digitalWrite(endSoundPin, LOW);
   delay(10);
-  digitalWrite(end_sound_pin, HIGH);
+  digitalWrite(endSoundPin, HIGH);
 }
 
 void playBackgroundMusic() {
-  digitalWrite(song_sound_pin, LOW);
-  digitalWrite(song_sound_pin, HIGH);
+  digitalWrite(songSoundPin, LOW);
+  digitalWrite(songSoundPin, HIGH);
 }
