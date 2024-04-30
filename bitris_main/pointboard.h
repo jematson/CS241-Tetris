2758b8b4108c8cfa40129078badc66bc21059f60
Elliott Lewandowski <elewandowski30@gmail.com>
2024-04-30 11:04:55 -0800

    Standerdized white space in pointboard.h

--- bitris_main/pointboard.h
+++ bitris_main/pointboard.h
@@ -4,8 +4,8 @@
 
 // Symbol Lookup Table
 const int symbols = 10;
-const unsigned int symbolTable[symbols] =
-{
+const unsigned int symbolTable[symbols] = {
+
   0b0000000011111100,  //0
   0b0000000001100000,  //1
   0b0000000011011010,  //2
@@ -19,8 +19,8 @@ const unsigned int symbolTable[symbols] =
 };
 
 const int digits = 4;
-const unsigned int digitTable[digits] =
-{
+const unsigned int digitTable[digits] = {
+
   0b1111000100000000, // 1st digit
   0b1111001000000000, // 2nd digit
   0b1111010000000000, // 3rd digit
@@ -31,6 +31,7 @@ const unsigned int digitTable[digits] =
 // 0b00004321ABCDEFGd
 // from a given digit and symbol
 unsigned int make_symbol_pattern(int digit, int symbol) {
+
   unsigned int digit_pattern = digitTable[digit - 1];
 
   unsigned int symbol_pattern = symbolTable[symbol];
@@ -39,6 +40,7 @@ unsigned int make_symbol_pattern(int digit, int symbol) {
 }
 
 void update_points_patterns() {
+
   int dig1 = (points / 1000) % 10;
   int dig2 = (points / 100) % 10;
   int dig3 = (points / 10) % 10;
@@ -47,6 +49,7 @@ void update_points_patterns() {
   int pointArray[] = {dig1, dig2, dig3, dig4};
 
   for(int i = 1; i < 5; ++i) {
+    
     unsigned int pattern = make_symbol_pattern(i, pointArray[i-1]);
     pointsPatterns[i-1] = pattern;
   }
